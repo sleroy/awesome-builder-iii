3ac626555c90d21e7f73582fc631bf0d87f0fb3f
Sylvain Leroy <sleroy@byoskill.com>
2023-04-26 18:53:53 +0200

    WIP

--- ui/README.md
+++ ui/README.md
@@ -1,57 +1,96 @@
-# Sendit
+<h1 align=center>Bigspring Light Astro</h1>
+<p align=center>Bigspring is a highly customizable Astro theme designed specifically for SaaS & marketing websites.  
+</p>
+<h2 align="center"> <a target="_blank" href="https://bigspring-light-astro.vercel.app/" rel="nofollow">Demo</a> | <a  target="_blank" href="https://pagespeed.web.dev/report?url=https%3A%2F%2Fbigspring-light-astro.vercel.app%2F&form_factor=desktop">Page Speed (100%)</a>
+</h2>
 
-Sendit is a polished, marketing website template for Astro. Browse through a [live demo](https://top-quail.cloudvent.net/).
+<p align=center>
+  <a href="https://github.com/withastro/astro/releases/tag/astro%402.0.11" alt="Contributors">
+    <img src="https://img.shields.io/static/v1?label=ASTRO&message=2.0&color=000&logo=astro" />
+  </a>
 
-![Sendit template screenshot](public/images/_screenshot.png)
+  <a href="https://github.com/themefisher/bigspring-light-astro/blob/main/LICENSE">
+    <img src="https://img.shields.io/github/license/themefisher/bigspring-light-astro" alt="license"></a>
 
-[![Deploy to CloudCannon](https://buttons.cloudcannon.com/deploy.svg)](https://app.cloudcannon.com/register#sites/connect/github/CloudCannon/sendit-astro-template)
+  <img src="https://img.shields.io/github/languages/code-size/themefisher/bigspring-light-astro" alt="code size">
 
-## Features
+  <a href="https://github.com/themefisher/bigspring-light-astro/graphs/contributors">
+    <img src="https://img.shields.io/github/contributors/themefisher/bigspring-light-astro" alt="contributors"></a>
+</p>
 
-- Pre-built pages
-- Pre-styled components
-- Blog with pagination and category pages
-- Configurable navigation and footer
-- Multiple hero options
-- Configurable theme colors
-- Optimised for editing in [CloudCannon](https://cloudcannon.com/)
+![bigspring-light](https://demo.gethugothemes.com/thumbnails/bigspring-light.png)
 
-## Setup
+Bigspring is a beautifully designed & highly customizable Astro theme created specifically for SaaS & marketing websites.  Users can customize the color scheme, fonts, menu, social links,  etc. and update the content easily based on their requirements. Bigspring is the perfect theme for showcasing your SaaS products or services and connecting with customers.
 
-1. Get a workflow going to see your site's output (with [CloudCannon](https://app.cloudcannon.com/)
-   or Astro locally).
 
-## Develop
+## Key Features
 
-Sendit is built with [Astro](https://astro.build/).
+- Highly customizable (Color, Font, Menu, Social Links, SEO Meta Tags, etc.)
+- Fast by default (95+ Google PageSpeed Score)
+- Netlify setting pre-configured
+- Contact form support
+- Pricing table
+- Support OG image
+- Write and update content in Markdown / MDX
+- MDX components auto import
+- Includes draft pages and posts
+- Built with Tailwind CSS framework
+- Fully responsive on desktops, tablets, and smartphones
+- SEO Friendly 
 
-```bash
-$ npm install
-$ npm run dev
+
+<!-- installation -->
+## Installation
+
+After downloading the template, you have some prerequisites to install. Then you can run it on your localhost. You can view the package.json file to see which scripts are included.
+
+### Install prerequisites (once for a machine)
+
+- **Node Installation:** [Install node js](https://nodejs.org/en/download/) [Recommended LTS version]
+
+### Local setup
+
+After successfully installing those dependencies, open this template with any IDE [[VS Code](https://code.visualstudio.com/) recommended], and then open the internal terminal of IDM [vs code shortcut <code>ctrl/cmd+\`</code>]
+
+- Install dependencies
+
+```
+npm install
 ```
 
-## Editing
+- Run locally
 
-Sendit is set up for adding, updating and removing pages, components, posts, portfolio items, company details and footer elements in [CloudCannon](https://app.cloudcannon.com/).
+```
+npm run dev
+```
 
-### Posts
+After that, it will open up a preview of the template in your default browser, watch for changes to source files, and live-reload the browser when changes are saved.
 
-- Add, update or remove a post in the _Posts_ collection.
-- The **Staff Author** field links to members in the **Staff** collection.
-- Change the defaults when new posts are created in `content/posts/_defaults.md`.
+## Production Build
 
-### Company details
+After finishing all the customization, you can create a production build by running this command.
 
-- Reused around the site to save multiple editing locations.
-- Set in the _Data_ / _Company_ section.
+```
+npm run build
+```
 
-### Nav/footer details
+<!-- reporting issue -->
+## Reporting Issues
 
-- Reused around the site to save multiple editing locations.
-- Set in the _Data_ section with respective names
+We use GitHub Issues as the official bug tracker for this Template. Please Search [existing issues](https://github.com/themefisher/bigspring-light-astro/issues). It’s possible someone has already reported the same problem.
+If your problem or idea has not been addressed yet, feel free to [open a new issue](https://github.com/themefisher/bigspring-light-astro/issues).
 
-### Theme colors
+<!-- licence -->
+## License
 
-- Theme colors can be set in _Data_ / _Theme_
-- The main colors are set and variants of them are computed
-- The colors will update on the next build
+Copyright (c) 2016 - Present, Designed & Developed by [Themefisher](https://themefisher.com)
+
+**Code License:** Released under the [MIT](https://github.com/themefisher/bigspring-light-astro/blob/main/LICENSE) license.
+
+**Image license:** The images are only for demonstration purposes. They have their license, we don't have permission to share those images.
+
+## Need Custom Development Services?
+
+Besides developing beautifully designed and blazing-fast themes, we help businesses create fast, performance-focused, scalable & secure websites based on NextJs, Hugo, Astro, etc.
+
+If you need a custom theme, theme customization, or complete website development services from scratch you can [Hire Us](https://themefisher.com/contact). 
--- ui/src/content/config.ts
+++ ui/src/content/config.ts
@@ -1,58 +1,175 @@
 import { defineCollection, z } from "astro:content";
 
-const seoSchema = z
-  .object({
-    page_description: z.string().nullable(),
-    canonical_url: z.string().nullable(),
-    featured_image: z.string().nullable(),
-    featured_image_alt: z.string().nullable(),
-    author_twitter_handle: z.string().nullable(),
-    open_graph_type: z.string().nullable(),
-    no_index: z.boolean(),
-  })
-  .optional();
-
-const blogCollection = defineCollection({
+// Homepage schema
+const homepage = defineCollection({
   schema: z.object({
-    date: z.date(),
-    title: z.string(),
-    tags: z.array(z.string()),
-    author: z.string(),
-    thumb_image: z.object({
+    banner: z.object({
+      title: z.string().optional(),
+      content: z.string().optional(),
+      image: z.string().optional(),
+      button: z.object({
+        label: z.string(),
+        link: z.string().default("#"),
+        enable: z.boolean().default(true)
+      })
+    }).optional(),
+    feature: z.object({
+      title: z.string().optional(),
+      features: z.array(z.object({name: z.string().optional(), icon: z.string().optional(), content: z.string().optional()})),
+    }).optional(),
+    services: z.array(z.object({
+      title: z.string().optional(),
+      content: z.string().optional(),
+      images: z.array(z.string()).optional(),
+      button: z.object({
+        label: z.string(),
+        link: z.string().default("#"),
+        enable: z.boolean().default(true)
+      }).optional()
+    })).optional(),
+    workflow: z.object({
+      title: z.string().optional(),
+      description: z.string().optional(),
+      image: z.string()
+    }).optional(),
+    call_to_action: z.object({
+      title: z.string().optional(),
+      content: z.string().optional(),
       image: z.string(),
-      image_alt: z.string(),
-    }),
-    featured_image: z.object({
-      image: z.string(),
-      image_alt: z.string(),
-    }),
-    seo: seoSchema,
+      button: z.object({
+        label: z.string(),
+        link: z.string().default("#"),
+        enable: z.boolean().default(true)
+      }).optional()
+    }).optional()
   }),
 });
 
-const pageSchema = z.object({
-  _schema: z.any().optional(),
-  title: z.string(),
-  content_blocks: z.array(z.any()),
-  page_size: z.undefined(),
-  description: z.undefined(),
-  seo: seoSchema,
+// Post collection schema
+const postsCollection = defineCollection({
+  schema: z.object({
+    id: z.string().optional(),
+    title: z.string(),
+    meta_title: z.string().optional(),
+    description: z.string().optional(),
+    date: z.date().optional(),
+    image: z.string().optional(),
+    authors: z.array(z.string()).default(["admin"]),
+    categories: z.array(z.string()).default(["others"]),
+    tags: z.array(z.string()).default(["others"]),
+    draft: z.boolean().optional(),
+  }),
 });
 
-const paginatedCollectionSchema = z.object({
-  _schema: z.literal("paginated_collection"),
-  title: z.string(),
-  description: z.string().optional(),
-  page_size: z.number().positive(),
-  content_blocks: z.undefined(),
-  seo: seoSchema,
+// Author collection schema
+const authorsCollection = defineCollection({
+  schema: z.object({
+    id: z.string().optional(),
+    title: z.string(),
+    meta_title: z.string().optional(),
+    email: z.string().optional(),
+    image: z.string().optional(),
+    description: z.string().optional(),
+    social: z
+      .object({
+        facebook: z.string().optional(),
+        twitter: z.string().optional(),
+        instagram: z.string().optional(),
+      })
+      .optional(),
+    draft: z.boolean().optional(),
+  }),
 });
 
+// Pages collection schema
 const pagesCollection = defineCollection({
-  schema: z.union([paginatedCollectionSchema, pageSchema]),
+  schema: z.object({
+    id: z.string().optional(),
+    title: z.string(),
+    meta_title: z.string().optional(),
+    description: z.string().optional(),
+    image: z.string().optional(),
+    layout: z.string().optional(),
+    draft: z.boolean().optional(),
+  }),
 });
+
+//Contact collection schema
+const contact_page = defineCollection({
+  schema: z.object({
+    title: z.string().optional(),
+    description: z.string().optional(),
+    draft: z.boolean().optional(),
+    info: z.object({
+      title: z.string().optional(),
+      description: z.string().optional(),
+      contacts: z.array(z.string()).optional()
+    }).optional()
+  })
+})
+
+//faq page schema
+const faq_page = defineCollection({
+  schema: z.object({
+    title: z.string().optional(),
+    description: z.string().optional(),
+    draft: z.boolean().optional(),
+    faqs: z.array(z.object({
+      title: z.string(),
+      answer: z.string(),
+    })).optional()
+  })
+})
+
+//pricing page schema
+const pricing_page = defineCollection({
+  schema: z.object({
+    title: z.string().optional(),
+    description: z.string().optional(),
+    draft: z.boolean().optional(),
+    plans: z.array(z.object({
+      title: z.string(),
+      subtitle: z.string(),
+      price: z.number(),
+      recommended: z.boolean().optional(),
+      type: z.string(),
+      features: z.array(z.string()).optional(),
+      button: z.object({
+        label: z.string(),
+        link: z.string().default("#"),
+        enable: z.boolean().default(true)
+      })
+    })).optional(),
+    call_to_action: z.object({
+      title: z.string().optional(),
+      content: z.string().optional(),
+      image: z.string(),
+      button: z.object({
+        label: z.string(),
+        link: z.string().default("#"),
+        enable: z.boolean().default(true)
+      }).optional()
+    }).optional()
+  })
+})
+// Export collections
+export interface PageData {
+  homePage: string,
+  posts: string,
+  pages: string,
+  authors: string,
+  contact: string,
+  faq: string,
+  pricing: string
+}
+
 
 export const collections = {
-  blog: blogCollection,
+  homepage: homepage,
+  posts: postsCollection,
   pages: pagesCollection,
+  authors: authorsCollection,
+  contact: contact_page,
+  faq: faq_page,
+  pricing: pricing_page
 };
